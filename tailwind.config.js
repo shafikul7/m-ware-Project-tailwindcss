08ed1e8b1496d512b1b7b5cfbddf4f30d2312e13
shafikul7 <shafikul1170@gmail.com>
2024-06-01 10:28:54 +0600

    update images

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,37 +3,34 @@ module.exports = {
   content: ["*"],
   theme: {
     colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'white': '#ffffff',
-      'primary': '#0060AF',
-      'secondary': '#00A88E',
-      'dark': '#333333',
-      'headerBgSm': '#002B4F',
-      'headerBgLg': '#333333',
-      'formBg': '#F9FAFB',
-      'hoverColor': '#13AF88',
-      'bannerBg': '#ECF5F8',
-      'paraColor':'#344054',
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      primary: "#0060AF",
+      secondary: "#00A88E",
+      dark: "#333333",
+      headerBgSm: "#002B4F",
+      headerBgLg: "#333333",
+      formBg: "#F9FAFB",
+      hoverColor: "#13AF88",
+      bannerBg: "#ECF5F8",
+      paraColor: "#344054",
     },
-extend: {
+    extend: {
+      backgroundImage: {
+        "footer-large": "url('/assets/footer.jpg')",
+        "footer-small": "url('/assets/background.jpg')",
+      },
 
-  backgroundImage: {
-    'footer-large': "url('/assets/Footer.png')",
-    'footer-small': "url('/assets/background.png')",
-  },
+      container: {
+        "max-width": "1696px",
+      },
 
-  container: {
-      "max-width": '1696px',  
+      fontSize: {
+        xyl: ["32px", "44px"],
+        xxl: ["80px", "90px"],
+      },
+    },
   },
-
-  fontSize: {
-    xyl: ['32px', '44px'],
-    xxl: ['80px', '90px'],
-  },
-},
- 
-},
   plugins: [],
-}
-
+};
